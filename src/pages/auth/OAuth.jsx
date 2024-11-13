58893c421ccc7ddbe8a08b73840f059235010d5f
Mayur Wagh <mayur2002wagh@gmail.com>
2024-11-13 18:38:38 +0530

    oAuth changes

--- src/pages/auth/OAuth.jsx
+++ src/pages/auth/OAuth.jsx
@@ -2,20 +2,26 @@ import React from "react";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 function OAuth({ service, icon, className, ...props }) {
-  const handelOAuth = () => {
+  // Ensure the service is correctly used
+  console.log(BASE_URL);
+  if (!service) {
+    console.error("OAuth service is undefined");
+    return null;
+  }
+
+  const handleOAuth = () => {
     window.location.href = `${BASE_URL}/auth/login/${service}`;
   };
 
   return (
     <button
       type="button"
-      onClick={handelOAuth}
-      className={` flex items-center p-3 w-full justify-center gap-5 rounded-lg  ${className}`}
+      onClick={handleOAuth}
+      className={`flex items-center p-3 w-full justify-center gap-5 rounded-lg ${className}`}
       {...props}
     >
       {icon}
-
-      <span className="">Continue with {service}</span>
+      <span>Continue with {service}</span>
     </button>
   );
 }
